ba44b81610e814f36e7cf1e81d70f9dcb2caff94
Oh yong Soo <dhdydtn2019@gmail.com>
2022-03-21 21:57:24 +0900

    feat (CommonResponse): 공통 응답 객체 생성

---
+++ src/main/java/com/cherrypick/backend/common/response/CommonResponse.java
@@ -0,0 +1,47 @@
+package com.cherrypick.backend.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> {
+
+  private Result result;
+  private T data;
+  private String message;
+
+  //status 200 + success (message가 있을 경우)
+  public static <T> CommonResponse success(T data, String message) {
+    return CommonResponse.builder()
+        .result(Result.SUCCESS)
+        .data(data)
+        .message(message)
+        .build();
+  }
+
+  //status 200 + success (message가 없을 경우)
+  public static <T> CommonResponse success(T data) {
+    return CommonResponse.builder()
+        .result(Result.SUCCESS)
+        .data(data)
+        .build();
+  }
+
+  //status 200 + fail
+  public static <T> CommonResponse fail(T data, String message) {
+    return CommonResponse.builder()
+        .result(Result.FAIL)
+        .data(data)
+        .message(message)
+        .build();
+  }
+
+  public enum Result {
+    SUCCESS, FAIL
+  }
+}
