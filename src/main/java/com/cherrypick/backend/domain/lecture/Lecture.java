aa285d6604d07d05e8b3e58e469c3f5a7ff30be4
Oh yong Soo <dhdydtn2019@gmail.com>
2022-03-29 23:11:58 +0900

    feat : 해시태그 컨버터 구현

--- src/main/java/com/cherrypick/backend/domain/lecture/Lecture.java
+++ src/main/java/com/cherrypick/backend/domain/lecture/Lecture.java
@@ -1,54 +1,66 @@
 package com.cherrypick.backend.domain.lecture;
 
-import com.cherrypick.backend.domain.bookmark.Bookmark;
-import com.cherrypick.backend.domain.review.Review;
+import static javax.persistence.FetchType.LAZY;
+
+import com.cherrypick.backend.domain.category.LectureCategory;
+import com.cherrypick.backend.domain.lecturecompany.LectureCompany;
+import com.cherrypick.backend.domain.lecturer.LectureLecturer;
+import com.cherrypick.backend.domain.lecturer.Lecturer;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import jdk.jfr.Unsigned;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Lecture {
 
   @Id
+  @Column(name = "id", nullable = false)
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private int price;
+  @OneToMany(mappedBy = "lecture")
+  private List<LectureCategory> lectureCategoryList = new ArrayList<>();
+
+  private String desktopImgUrl;
+
+  private String tabletImgUrl;
+
+  private String mobileImgUrl;
 
   private String name;
 
-  private Long platformId;
+  private boolean isOffline;
 
-  private String lectureLink;
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "lecture_company_id")
+  private LectureCompany lectureCompany;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "lecture_hashtag_id")
-  private LectureHashtag lectureHashtag;
+  @OneToMany(mappedBy = "lecture" )
+  private List<LectureLecturer> lecturer = new ArrayList<>();
 
-  private Boolean isOffline;
+  @Convert(converter = HashTagConverter.class)
+  private List<String> hashTagList = new ArrayList<>();
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "bookmark_id")
-  private Bookmark bookmark;
+  private String originLink;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "lecture_lecturer_id")
-  private LectureLecturer lectureLecturer;
+  private int price;
 
-  private String lectureInfo;
+  private String info;
 
-  @OneToMany(mappedBy = "lecture")
-  private List<Review> reviews = new ArrayList<>();
+  private boolean isOpened;
+
 }
