a4a899c3745860b5d450eaa22658428b2d6f468b
Oh yong Soo <dhdydtn2019@gmail.com>
2022-07-06 01:58:35 +0900

    refactor: user 조회 로직 추가

--- src/main/java/com/cherrypick/backend/domain/review/ReviewCommand.java
+++ src/main/java/com/cherrypick/backend/domain/review/ReviewCommand.java
@@ -2,14 +2,16 @@ package com.cherrypick.backend.domain.review;
 
 import com.cherrypick.backend.domain.lecture.Lecture;
 import com.cherrypick.backend.presentation.review.ReviewDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class ReviewCommand {
 
   @Getter
+  @AllArgsConstructor
   public static class RegisterRequest {
 
-    private final String name;
+    private final String loginId;
     private final long lectureId;
     private final double rating;
     private final Review.Recommendation recommendation;
@@ -18,8 +20,8 @@ public class ReviewCommand {
     private final String strengthComment;
     private final String weaknessComment;
 
-    public RegisterRequest(String name, Long lectureId, ReviewDto.RegisterRequest request) {
-      this.name = name;
+    public RegisterRequest(String loginId, Long lectureId, ReviewDto.RegisterRequest request) {
+      this.loginId = loginId;
       this.lectureId = lectureId;
       this.rating = request.getRating();
       this.recommendation = request.getRecommendation();
@@ -29,10 +31,10 @@ public class ReviewCommand {
       this.weaknessComment = request.getWeaknessComment();
     }
 
-    public Review toEntity(Lecture lecture) {
+    public Review toEntity(Lecture lecture, Long userId) {
       return Review.of(lecture, rating, recommendation, costPerformance, oneLineComment,
         strengthComment,
-        weaknessComment, lectureId);
+        weaknessComment, userId);
     }
   }
 }
--- src/main/java/com/cherrypick/backend/domain/review/ReviewServiceImpl.java
+++ src/main/java/com/cherrypick/backend/domain/review/ReviewServiceImpl.java
@@ -5,6 +5,8 @@ import com.cherrypick.backend.common.exception.ErrorCode;
 import com.cherrypick.backend.domain.lecture.Lecture;
 import com.cherrypick.backend.domain.lecture.LectureReader;
 import com.cherrypick.backend.domain.review.ReviewInfo.ReviewStatistics;
+import com.cherrypick.backend.domain.user.User;
+import com.cherrypick.backend.domain.user.UserReader;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ public class ReviewServiceImpl implements ReviewService {
   private final ReviewReader reviewReader;
   private final ReviewStore reviewStore;
   private final LectureReader lectureReader;
+  private final UserReader userReader;
 
 
   @Override
@@ -36,9 +39,13 @@ public class ReviewServiceImpl implements ReviewService {
   @Override
   public void createReview(ReviewCommand.RegisterRequest command) {
     long lectureId = command.getLectureId();
+    String loginId = command.getLoginId();
+
+    User user = userReader.findByProviderId(loginId).orElseThrow(
+      () -> new BusinessException(loginId + "사용자를 찾지 못했습니다.", ErrorCode.NOT_FOUND_USER));
     Lecture lecture = lectureReader.findByLectureId(lectureId)
       .orElseThrow(() -> new BusinessException(lectureId + "강의를 찾지 못했습니다.",
         ErrorCode.NOT_FOUND_LECTURE));
-    reviewStore.store(command.toEntity(lecture));
+    reviewStore.store(command.toEntity(lecture, user.getId()));
   }
 }
