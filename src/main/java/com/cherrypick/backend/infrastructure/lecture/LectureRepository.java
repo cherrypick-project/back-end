6d7725b1900bab478d800c97816ec7bf56c516ef
sinkyoungdeok <tkagmd1@naver.com>
2022-07-16 17:11:49 +0900

    repositoryimpl 취소

--- src/main/java/com/cherrypick/backend/infrastructure/lecture/LectureRepository.java
+++ src/main/java/com/cherrypick/backend/infrastructure/lecture/LectureRepository.java
@@ -3,6 +3,6 @@ package com.cherrypick.backend.infrastructure.lecture;
 import com.cherrypick.backend.domain.lecture.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LectureRepository extends JpaRepository<Lecture, Long>, LectureRepositoryCustom {
+public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
 }
